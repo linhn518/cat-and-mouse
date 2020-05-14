e3f1e0d121a47a3b609816b362eeef6aacdf35cd
linhn518 <linhn518@gmail.com>
2020-05-14 11:02:22 +0300

    Add Cat and Mouse file

--- Cat.js
+++ Cat.js
@@ -1,7 +1,12 @@
 function Cat(name){
     this.name = name;
+    this.stomach = [];
     console.log('Hi! I am' + this.name);
 }
 
+Cat.prototype.eat= function(){
+    this.stomach.push(Mouse);
+}
 Cat('Tom');
+module.exports = Cat;
 
--- Mouse.js
+++ Mouse.js
@@ -1,3 +1,10 @@
-function(){
-    console.log('Hello! I am Mouse');
+function Mouse(name){
+    this.name= name;
+    this.dead= false; 
 }
+
+Mouse.prototype.die= function(){
+    this.dead= true
+};
+
+module.exports= Mouse;
