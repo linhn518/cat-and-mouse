dd8ff59340ddc8498c5be61b86d903f4af2e1c6f
linhn518 <linhn518@gmail.com>
2020-05-14 13:37:17 +0300

    a little change

--- Dog.js
+++ Dog.js
@@ -1,9 +1,18 @@
+var Chalk = require('chalk');
+var Cat = require('./Cat');
+var Mouse = require('./Mouse');
+
 function Dog(name){
     this.name= name;
     this.stomach= [];
+    console.log('I am a Dog. My name is '+ Chalk.blue(this.name));
 }
 
-Dog.prototype.eat= function(){
+var pug = new Dog('Pug');
+var tom = new Cat('Tom');
+var jerry = new Mouse('Jerry');
+
+Dog.prototype.eat = function(){
     this.stomach.push(Mouse);
 }
 
